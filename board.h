a51b55562a4275b9f03a34c0651e77dbbc374521
taeheon5886 <taeheon5886@snu.ac.kr>
2023-05-05 18:49:10 +0900

    second commit

--- board.h
+++ board.h
@@ -5,37 +5,39 @@
 using std::endl;
 using std::ofstream;
 
+
 class Board {
-    public:
-        Board(int num_jobs, int width, int height, ofstream& output_stream);
-        ~Board();
+public:
+    Board(int num_jobs, int width, int height, ofstream& output_stream);
+    ~Board();
 
-        void print_board();
-        void print_job(int job_idx, char job_type, int id);
+    void print_board(int x, int y);
+    void print_job(int job_idx, char job_type, int id);
 
-        //job functions
-        void insert_page(int x, int y, int width, int height, int id, int content);
-        void delete_page(int id);
-        void modify_content(int id, char content);
-        void modify_position(int id, int x, int y);
+    //job functions
+    void insert_page(int x, int y, int width, int height, int id, char content);
+    void delete_page(int id);
+    void modify_content(int id, char content);
+    void modify_position(int id, int x, int y);
+    friend class page;
 
-    private:
-        int num_jobs, width, height; 
-        ofstream& output; 
-        char* board; 
+private:
+    int num_jobs, width, height;
+    ofstream& output;
+    char* board;
 };
 
 
-Board::Board(int num_jobs, int width, int height, ofstream& output_stream): output(output_stream) {
+Board::Board(int num_jobs, int width, int height, ofstream& output_stream) : output(output_stream) {
     this->width = width;
     this->height = height;
     this->num_jobs = num_jobs;
 
-    board = new char[width*height];
+    board = new char[width * height];
 
     for (int h = 0; h < height; h++) {
         for (int w = 0; w < width; w++) {
-            board[h*width + w] = ' ';
+            board[h * width + w] = ' ';
         }
     }
 
@@ -43,59 +45,68 @@ Board::Board(int num_jobs, int width, int height, ofstream& output_stream): outp
 
 Board::~Board() {
     delete board;
-    
+
 }
 
 
-void Board::print_board() {
+void Board::print_board(int x, int y) {
     int h, w;
-    for (w = 0; w < width+2; w++) output << "- ";
+    for (w = 0; w < width + 2; w++) output << "- ";
     output << endl;
-    
+
     for (h = 0; h < height; h++) {
         output << "| ";
         for (w = 0; w < width; w++) {
-            output << board[h*width + w] << " ";
+            output << board[h * width + w] << " ";
         }
         output << "| " << endl;
     }
-
-    for (w = 0; w < width+2; w++) output << "- ";
+    for (w = 0; w < width + 2; w++) output << "- ";
     output << endl;
 }
 
 void Board::print_job(int job_idx, char job_type, int id) {
-    output << ">> (" << job_idx <<") ";
-    switch(job_type) {
-        
-        case 'i':
-            output << "Insert ";
-            break;
-        case 'd':
-            output << "Delete ";
-            break;
-        case 'm':
-            output << "Modify ";
-            break;
+
+    output << ">> (" << job_idx << ") ";
+    switch (job_type) {
+
+    case 'i':
+        output << "Insert ";
+        break;
+    case 'd':
+        output << "Delete ";
+        break;
+    case 'm':
+        output << "Modify ";
+        break;
     }
 
     output << id << endl;
 }
 
 
-void Board::insert_page(int x, int y, int width, int height, int id, int content) {
+void Board::insert_page(int x, int y, int page_width, int page_height, int id, char content)//insert_page(0,1,10,15,c)
+{
 
+    for (int h = y; h < y + page_height; h++) {
+        for (int w = x; w < x + page_width; w++) {
+            board[h * width + w] = content;
+        }
+    }
+    print_board(x, y);
 }
 
 void Board::delete_page(int id) {
-    
+
 }
 
 void Board::modify_content(int id, char content) {
-   
+
 
 }
 void Board::modify_position(int id, int x, int y) {
-   
-    
+
+
+
 }
+// github 연동 체크 중
--- main.cpp
+++ main.cpp
@@ -6,92 +6,93 @@
 
 using namespace std;
 
-void manage_board(string input_file, string output_file) {
-    
+void manage_board1() {
+
+
     int width, height;
-    int num_jobs; 
+    int num_jobs;
     char job_type;
     ifstream input;
     ofstream output;
 
-    input.open(input_file);
+    input.open("input_0.txt");
 
     if (!(input.is_open())) {
-        cerr << "Input missing" << endl;
-        return;
-    }    
-    
-    input >> width >> height >> num_jobs;
-
-    output.open(output_file, ofstream::trunc);
-
-    if (!(output.is_open())) {
-        cerr << "Output missing" << endl;
+        cout << "Input missing" << endl;
         return;
     }
 
+    input >> width >> height >> num_jobs;
+    output.open("outputt.txt", ofstream::trunc);
 
-    Board board = Board(num_jobs, width, height, output);
+    if (!(output.is_open())) {
+        cout << "Output missing" << endl;
+        return;
+    }
+
+    Board board = Board(num_jobs, width, height, output); //board.num_jobs=1, width=50, height=20;ofstream& output=output;
+
 
 
     int page_x, page_y, page_width, page_height, page_id;
     char page_content;
-    
-    for (int job_idx = 1; job_idx <= num_jobs; job_idx++) {
+
+    for (int job_idx = 1; job_idx <= 4; job_idx++) {
         input >> job_type;
-
         switch (job_type) {
-            case 'i': 
-                //insert page
-                input >> page_id >> page_x >> page_y >> page_width >> page_height >> page_content;
-                board.print_job(job_idx, job_type, page_id);
-                board.insert_page(page_x, page_y, page_width, page_height, page_id, page_content);      
-                break;
-            case 'd':
-                //delete page
-                input >> page_id;
-                board.print_job(job_idx, job_type, page_id);
-                board.delete_page(page_id);
-                break;
-            case 'm':
-                //modify page
-                int modify_content;
-                input >> modify_content;
+        case 'i':
+            //insert page
+            input >> page_id >> page_x >> page_y >> page_width >> page_height >> page_content;//id=8, x=0 y=1 width =10 height = 15 content =c
+            board.print_job(job_idx, job_type, page_id); //1,i,8
+            board.insert_page(page_x, page_y, page_width, page_height, page_id, page_content);      //insert_page(0,1,10,15,c)
+            break;
+        case 'd':
+            //delete page
+            input >> page_id;
+            board.print_job(job_idx, job_type, page_id);
+            board.delete_page(page_id);
+            break;
+        case 'm':
+            //modify page
+            int modify_content;
+            input >> modify_content;
 
-                if (modify_content) {
-                    //modify content 
-                    input >> page_id >> page_content;
-                    board.print_job(job_idx, job_type, page_id);
-                    board.modify_content(page_id, page_content);
-                } else {
-                    //modify position
-                    input >> page_id >> page_x >> page_y;
-                    board.print_job(job_idx, job_type, page_id);
-                    board.modify_position(page_id, page_x, page_y);
-                }
-                break;
-            default:
-                cerr << "Wrong job type" << endl;
-                return;
+            if (modify_content) {
+                //modify content 
+                input >> page_id >> page_content;
+                board.print_job(job_idx, job_type, page_id);
+                board.modify_content(page_id, page_content);
+            }
+            else {
+                //modify position
+                input >> page_id >> page_x >> page_y;
+                board.print_job(job_idx, job_type, page_id);
+                board.modify_position(page_id, page_x, page_y);
+            }
+            break;
+        default:
+            cout << "Wrong job type" << endl;
+            return;
         }
 
-        cerr << "Job " << job_idx << " Complete!" << endl;
+        cout << "Job " << job_idx << " Complete!" << endl;
 
-    } 
-    
+    }
+
 
     input.close();
     output.close();
-    
+
 }
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
-    if (argc==3) manage_board(string(argv[1]), string(argv[2]));
-    else cerr << "Wrong arguments" << endl;
-    
+    manage_board1();
+    //if (argc==3) manage_board(string(argv[1]), string(argv[2]));
+    // else cerr << "Wrong arguments" << endl;
 
-    return 0; 
+
+    return 0;
 
 }
